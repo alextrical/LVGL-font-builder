511b2a7fbfc1c34a00cc23c9eaffbdb75e80de52
alextrical <35117191+alextrical@users.noreply.github.com>
2026-05-03 20:31:56 +0100

    Add special characters

--- scripts/built_in_font/generate_all.py
+++ scripts/built_in_font/generate_all.py
@@ -3,67 +3,67 @@
 import os
 
 print("Generating 8 px")
-os.system("./built_in_font_gen.py --size 8 -o lv_font_montserrat_8.c --bpp 4")
+os.system("./built_in_font_gen.py --size 8 -s ÄäÖöÜüß° -o lv_font_montserrat_8.c --bpp 4")
 
 print("\nGenerating 10 px")
-os.system("./built_in_font_gen.py --size 10 -o lv_font_montserrat_10.c --bpp 4")
+os.system("./built_in_font_gen.py --size 10 -s ÄäÖöÜüß° -o lv_font_montserrat_10.c --bpp 4")
 
 print("\nGenerating 12 px")
-os.system("./built_in_font_gen.py --size 12 -o lv_font_montserrat_12.c --bpp 4")
+os.system("./built_in_font_gen.py --size 12 -s ÄäÖöÜüß° -o lv_font_montserrat_12.c --bpp 4")
 
 print("\nGenerating 14 px")
-os.system("./built_in_font_gen.py --size 14 -o lv_font_montserrat_14.c --bpp 4")
+os.system("./built_in_font_gen.py --size 14 -s ÄäÖöÜüß° -o lv_font_montserrat_14.c --bpp 4")
 
 print("\nGenerating 16 px")
-os.system("./built_in_font_gen.py --size 16 -o lv_font_montserrat_16.c --bpp 4")
+os.system("./built_in_font_gen.py --size 16 -s ÄäÖöÜüß° -o lv_font_montserrat_16.c --bpp 4")
 
 print("\nGenerating 18 px")
-os.system("./built_in_font_gen.py --size 18 -o lv_font_montserrat_18.c --bpp 4")
+os.system("./built_in_font_gen.py --size 18 -s ÄäÖöÜüß° -o lv_font_montserrat_18.c --bpp 4")
 
 print("\nGenerating 20 px")
-os.system("./built_in_font_gen.py --size 20 -o lv_font_montserrat_20.c --bpp 4")
+os.system("./built_in_font_gen.py --size 20 -s ÄäÖöÜüß° -o lv_font_montserrat_20.c --bpp 4")
 
 print("\nGenerating 22 px")
-os.system("./built_in_font_gen.py --size 22 -o lv_font_montserrat_22.c --bpp 4")
+os.system("./built_in_font_gen.py --size 22 -s ÄäÖöÜüß° -o lv_font_montserrat_22.c --bpp 4")
 
 print("\nGenerating 24 px")
-os.system("./built_in_font_gen.py --size 24 -o lv_font_montserrat_24.c --bpp 4")
+os.system("./built_in_font_gen.py --size 24 -s ÄäÖöÜüß° -o lv_font_montserrat_24.c --bpp 4")
 
 print("\nGenerating 26 px")
-os.system("./built_in_font_gen.py --size 26 -o lv_font_montserrat_26.c --bpp 4")
+os.system("./built_in_font_gen.py --size 26 -s ÄäÖöÜüß° -o lv_font_montserrat_26.c --bpp 4")
 
 print("\nGenerating 28 px")
-os.system("./built_in_font_gen.py --size 28 -o lv_font_montserrat_28.c --bpp 4")
+os.system("./built_in_font_gen.py --size 28 -s ÄäÖöÜüß° -o lv_font_montserrat_28.c --bpp 4")
 
 print("\nGenerating 30 px")
-os.system("./built_in_font_gen.py --size 30 -o lv_font_montserrat_30.c --bpp 4")
+os.system("./built_in_font_gen.py --size 30 -s ÄäÖöÜüß° -o lv_font_montserrat_30.c --bpp 4")
 
 print("\nGenerating 32 px")
-os.system("./built_in_font_gen.py --size 32 -o lv_font_montserrat_32.c --bpp 4")
+os.system("./built_in_font_gen.py --size 32 -s ÄäÖöÜüß° -o lv_font_montserrat_32.c --bpp 4")
 
 print("\nGenerating 34 px")
-os.system("./built_in_font_gen.py --size 34 -o lv_font_montserrat_34.c --bpp 4")
+os.system("./built_in_font_gen.py --size 34 -s ÄäÖöÜüß° -o lv_font_montserrat_34.c --bpp 4")
 
 print("\nGenerating 36 px")
-os.system("./built_in_font_gen.py --size 36 -o lv_font_montserrat_36.c --bpp 4")
+os.system("./built_in_font_gen.py --size 36 -s ÄäÖöÜüß° -o lv_font_montserrat_36.c --bpp 4")
 
 print("\nGenerating 38 px")
-os.system("./built_in_font_gen.py --size 38 -o lv_font_montserrat_38.c --bpp 4")
+os.system("./built_in_font_gen.py --size 38 -s ÄäÖöÜüß° -o lv_font_montserrat_38.c --bpp 4")
 
 print("\nGenerating 40 px")
-os.system("./built_in_font_gen.py --size 40 -o lv_font_montserrat_40.c --bpp 4")
+os.system("./built_in_font_gen.py --size 40 -s ÄäÖöÜüß° -o lv_font_montserrat_40.c --bpp 4")
 
 print("\nGenerating 42 px")
-os.system("./built_in_font_gen.py --size 42 -o lv_font_montserrat_42.c --bpp 4")
+os.system("./built_in_font_gen.py --size 42 -s ÄäÖöÜüß° -o lv_font_montserrat_42.c --bpp 4")
 
 print("\nGenerating 44 px")
-os.system("./built_in_font_gen.py --size 44 -o lv_font_montserrat_44.c --bpp 4")
+os.system("./built_in_font_gen.py --size 44 -s ÄäÖöÜüß° -o lv_font_montserrat_44.c --bpp 4")
 
 print("\nGenerating 46 px")
-os.system("./built_in_font_gen.py --size 46 -o lv_font_montserrat_46.c --bpp 4")
+os.system("./built_in_font_gen.py --size 46 -s ÄäÖöÜüß° -o lv_font_montserrat_46.c --bpp 4")
 
 print("\nGenerating 48 px")
-os.system("./built_in_font_gen.py --size 48 -o lv_font_montserrat_48.c --bpp 4")
+os.system("./built_in_font_gen.py --size 48 -s ÄäÖöÜüß° -o lv_font_montserrat_48.c --bpp 4")
 
 # print("\nGenerating 12 px subpx")
 # os.system("./built_in_font_gen.py --size 12 -o lv_font_montserrat_12_subpx.c --bpp 4 --subpx")
